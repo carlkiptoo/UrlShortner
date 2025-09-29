71cd148715355fa48655ace2fd67f45ac0482c5a
carlkiptoo <carlosiamcarl@gmail.com>
2025-09-29 16:49:00 +0300

    shortener endpoint

---
+++ src/routes/shorten.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { pool } from '../config/db';
+import { encodeBase62 } from '../utils/base62';
+
+const router = Router();
+
+router.post('/', async (req: any, res: any) => {
+    const {long_url} = req.body;
+
+    if (!long_url) {
+        return res.status(400).json({error: 'long_url is required'});
+    }
+
+    const client = await pool.connect();
+
+    try {
+        await client.query('BEGIN');
+        const insert = await client.query(
+            "INSERT INTO urls (long_url, short_code) VALUES ($1, '') RETURNING id",
+            [long_url]
+        );
+
+        const id = insert.rows[0].id;
+        const short_code = encodeBase62(id);
+
+        await client.query("UPDATE urls SET short_code = $1 WHERE id = $2", [short_code, id]);
+        await client.query('COMMIT');
+
+        res.json({short_code: short_code, short_url: `http://localhost:3000/${short_code}`});
+    } catch (error) {
+        await client.query('ROLLBACK');
+        console.error(error);
+        res.status(500).json({error: 'Internal Server Error'});
+    } finally {
+        client.release();
+    }
+
+});
+
+export default router;
