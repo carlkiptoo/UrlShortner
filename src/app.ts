c5022b689dcaa79f36eea34c344df0f2275c83c7
carlkiptoo <carlosiamcarl@gmail.com>
2025-10-01 10:57:51 +0300

    Added logging and monitoring

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,27 @@ import express from 'express';
 import shortenRouter from './routes/shorten.js';
 import redirectRouter from './routes/redirect.js';
 import {limiter} from './middleware/rateLimiter.js';
+import morgan from 'morgan';
+import {logger} from './utils/logger.js';
+
 
 const app = express();
 
 app.use(express.json());
+
+app.use(morgan('combined', {
+    skip: (req: express.Request) => req.url === '/api/live/ws',
+    stream: {
+        write: (message: any) => logger.info(message.trim()),
+    },
+}));
+
 app.use('/api/shorten', limiter, shortenRouter);
 app.use('/', limiter, redirectRouter);
+
+app.use((err: any, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
+    logger.error(`${err.message}\n${err.stack}`);
+    res.status(500).json({error: 'Internal Server Error'});
+})
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
-import app from './app.js';
+import app from './app';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
